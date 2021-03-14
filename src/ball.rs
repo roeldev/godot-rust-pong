9556884dd183b2c1412ac5875d2d9c3eee2517aa
roeldev <roeldev@users.noreply.github.com>
2021-03-14 17:05:01 +0100

    Add copyright notices

--- src/ball.rs
+++ src/ball.rs
@@ -1,3 +1,7 @@
+// Copyright (c) 2021, Roel Schut. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 use crate::{utils::find_node};
 use gdnative::prelude::*;
 use gdnative::api::{KinematicBody2D, OS, AudioStreamPlayer};
